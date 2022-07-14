7ca5755b9281a7ef6a38623dd66b6bf3d5d987c0
renatohvo <renatohvo@gmail.com>
2022-07-14 18:31:43 -0300

    Test Profile and H2 Seeding

--- backend/src/main/java/com/renatohvo/rhvoproducts/services/CategoryService.java
+++ backend/src/main/java/com/renatohvo/rhvoproducts/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.renatohvo.rhvoproducts.entities.Category;
 import com.renatohvo.rhvoproducts.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Notebooks');
+INSERT INTO tb_category (name) VALUES ('Tablets');
+INSERT INTO tb_category (name) VALUES ('Drawing Tablets');
+INSERT INTO tb_category (name) VALUES ('Smartphones');
