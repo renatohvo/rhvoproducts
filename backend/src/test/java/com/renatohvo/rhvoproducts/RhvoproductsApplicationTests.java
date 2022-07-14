1dc6296a058f4a8757beba0fadea8451a5ed4c03
renatohvo <renatohvo@gmail.com>
2022-07-14 13:42:31 -0300

    First Commit

---
+++ backend/src/test/java/com/renatohvo/rhvoproducts/RhvoproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatohvo.rhvoproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhvoproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
