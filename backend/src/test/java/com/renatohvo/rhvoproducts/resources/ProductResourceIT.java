8a53881ba78680afd0af2132ec2ab564f468f15b
renatohvo <renatohvo@gmail.com>
2022-10-26 20:11:10 -0300

    ProductResourceIT->>Integration Testing

---
+++ backend/src/test/java/com/renatohvo/rhvoproducts/resources/ProductResourceIT.java
@@ -0,0 +1,98 @@
+package com.renatohvo.rhvoproducts.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.renatohvo.rhvoproducts.dto.ProductDTO;
+import com.renatohvo.rhvoproducts.tests.Factory;
+
+@SpringBootTest
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private Long idExist;
+	private Long idNotExist;
+	private Long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		idExist = 1L;
+		idNotExist = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+		
+		ProductDTO productDTO = Factory.createProductDTO();
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		String expectedName = productDTO.getName();
+		String expectedDescription = productDTO.getDescription();
+		
+		ResultActions result = 
+				mockMvc.perform(put("/products/{id}", idExist)
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(idExist));
+		result.andExpect(jsonPath("$.name").value(expectedName));
+		result.andExpect(jsonPath("$.description").value(expectedDescription));
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdNotExists() throws Exception {
+		
+		ProductDTO productDTO = Factory.createProductDTO();
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		ResultActions result = 
+				mockMvc.perform(put("/products/{id}", idNotExist)
+						.content(jsonBody)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+						.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+		result.andExpect(jsonPath("$.content").exists());
+		result.andExpect(jsonPath("$.content[0].name").value("Cell Phone"));
+		result.andExpect(jsonPath("$.content[1].name").value("Lego"));
+		result.andExpect(jsonPath("$.content[2].name").value("Notebook A"));
+	}
+	
+}
