641b1f1cb86f90b8ad2b93a69162b1df749483f5
renatohvo <renatohvo@gmail.com>
2023-01-19 00:26:40 -0300

    Security + UserDetails + UserDetailsService

--- backend/src/main/java/com/renatohvo/rhvoproducts/services/UserService.java
+++ backend/src/main/java/com/renatohvo/rhvoproducts/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +30,9 @@ import com.renatohvo.rhvoproducts.services.exceptions.DatabaseException;
 import com.renatohvo.rhvoproducts.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private BCryptPasswordEncoder passEncoder;
@@ -93,6 +100,18 @@ public class UserService {
 			Role role = roleRepository.getOne(roleDto.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userRepository.findByEmail(username);
+		if(user == null) {
+			logger.error("USER NOT FOUND: " + username);
+			throw new UsernameNotFoundException("EMAIL NOT FOUND.");
+		}
+		logger.info("USER FOUND: " + username);
+		return user;
 	}
 	
 }
